c63a95d5dfa2e29b8140af14bcd46944634a52d9
James Fenn <me@jfenn.me>
2023-02-05 10:53:31 -0500

    skip skybox/clouds rendering

---
+++ src/main/java/me/jfenn/lidar/mixin/GameRendererMixin.java
@@ -0,0 +1,8 @@
+package me.jfenn.lidar.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+}
--- src/main/java/me/jfenn/lidar/mixin/WorldRendererMixin.java
+++ src/main/java/me/jfenn/lidar/mixin/WorldRendererMixin.java
@@ -1,23 +1,14 @@
 package me.jfenn.lidar.mixin;
 
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.Entity;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.math.Matrix4f;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.profiler.Profiler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
@@ -28,11 +19,23 @@ public abstract class WorldRendererMixin {
             locals = LocalCapture.PRINT
     )
     private void skipEntities(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
-
+        // TODO: does not properly skip entity rendering
     }
 
     @Overwrite
     private void renderLayer(RenderLayer renderLayer, MatrixStack matrices, double cameraX, double cameraY, double cameraZ, Matrix4f positionMatrix) {
+        // skips all block rendering
+    }
+
+    @Inject(method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V", at = @At("HEAD"), cancellable = true)
+    private void renderSky(MatrixStack matrices, Matrix4f matrix4f, float tickDelta, Camera camera, boolean bl, Runnable runnable, CallbackInfo ci) {
+        runnable.run();
+        ci.cancel();
+    }
+
+    @Inject(method = "renderClouds(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/util/math/Matrix4f;FDDD)V", at = @At("HEAD"), cancellable = true)
+    private void renderClouds(MatrixStack matrices, Matrix4f matrix4f, float tickDelta, double d, double e, double f, CallbackInfo ci) {
+        ci.cancel();
     }
 
 }
